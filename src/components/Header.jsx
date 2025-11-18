0c707e8e16dd7247e41336cb8e1c7f38345c9da7
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-18 16:25:04 +0100

    complete milestone 2, add btn in header and use the context here

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
+import { useContext } from "react";
+import BudgetContext from '../contexts/BudgetContext'
 
 export default function Header() {
+
+    const { budgetMode, setBudgetMode } = useContext(BudgetContext)
+
+
 
     return (
         <nav className="navbar navbar-expand-lg bg-first">
             <div className="container-fluid container-lg py-2">
                 <Logo isHeader={true} />
-                <div className="navbar-links d-flex text-white fs-5 gap-4 fw-bold">
+                <div className="navbar-links d-flex text-white fs-5 gap-4 fw-bold align-items-center">
                     <NavLink className="nav-link" to="/">Home</NavLink>
                     <NavLink className="nav-link" to="/contacts">Chi Siamo</NavLink>
                     <NavLink className="nav-link" to="/products">Prodotti</NavLink>
                     <NavLink className="nav-link" to="/faqs">Faqs</NavLink>
+                    <button className={`btn py-2 ${budgetMode ? "btn-danger" : "btn-success"}`} onClick={() => setBudgetMode(!budgetMode)}>{budgetMode ? "Normal Mode" : "Budget Mode"}</button>
                 </div>
             </div>
         </nav>
